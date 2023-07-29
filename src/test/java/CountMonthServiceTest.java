1f4aa3ed498209417d372813764d318e4572785e
Liza go <elizaveta2000333@gmail.com>
2023-07-29 20:01:06 +0300

    testcsv

--- src/test/java/CountMonthServiceTest.java
+++ src/test/java/CountMonthServiceTest.java
@@ -1,25 +1,18 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import ru.netology.javaloops.services.CountMonthService;
 
 public class CountMonthServiceTest {
 
-    @Test
-    public void test1 () {
+    @ParameterizedTest
+   @CsvFileSource(files ="src/test/resources/count.csv")
+    public void test (int expected, int income, int expenses, int threshold) {
         CountMonthService service = new CountMonthService();
 
-        int expected = 3;
-        int actual = service.calculate(10000, 3000, 20000);
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void test2 () {
-        CountMonthService service = new CountMonthService();
-
-        int expected = 2;
-        int actual = service.calculate(100000, 60000, 150000);
+        int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
